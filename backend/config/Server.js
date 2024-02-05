745afe85cc0bfd77ab64669602bd4b0189680344
alpinocode <alfinohasan629@gmail.com>
2024-02-05 19:39:31 +0700

    change in db connect to db

--- backend/config/Server.js
+++ backend/config/Server.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
 
 
-const dbConect = new Sequelize('book', 'root', '', {
+const db = new Sequelize('book', 'root', '', {
     host: process.env.HOST,
     port: 8111,
     dialect: 'mysql'
 })
 
-export default dbConect
+export default db
