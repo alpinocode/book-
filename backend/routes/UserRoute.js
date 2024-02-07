f642d82def2efc875d44b70f8219c94e03059258
alpinocode <alfinohasan629@gmail.com>
2024-02-07 23:24:11 +0700

    route function call to the router

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,12 +1,23 @@
 import express from 'express'
 import {
     getUser,
-    Register
+    Register,
+    Login
 } from '../controllers/UserController.js'
+import {
+    RefreshToken
+} from '../controllers/RefreshToken.js'
+import { 
+    VerifyToken
+} from '../middleware/VerifyToken.js'
+
 
 const router = express.Router()
 
-router.get('/user', getUser)
-router.post('/register',  Register )
+router.get('/user', VerifyToken,getUser)
+router.post('/register', Register )
+router.post('/login', Login)
+router.get('/token', RefreshToken)
+
 
 export default router
