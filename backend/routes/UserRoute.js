87d625e4ea2c14875e4b94c3d51aae1cf0f823bd
alpinocode <alfinohasan629@gmail.com>
2024-02-11 21:50:30 +0700

    remove route errors

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -22,7 +22,7 @@ import {
 
 const router = express.Router()
 
-router.get('/user', VerifyToken,getUser, getNote, getNoteById, createNote, noteUpdate, noteDelete)
+router.get('/user', VerifyToken,getUser)
 router.post('/register', Register )
 router.post('/login', Login)
 router.get('/token', RefreshToken)
